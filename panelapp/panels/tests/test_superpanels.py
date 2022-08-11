eac1c8e0038c7049ec5ac7609f20ca9d19f7349c
Alasdair Colley <alasdair.colley@genomicsengland.co.uk>
2022-08-11 11:00:53 +0000

    test: add expected failing test for known bug

--- panelapp/panels/tests/test_superpanels.py
+++ panelapp/panels/tests/test_superpanels.py
@@ -21,8 +21,13 @@
 ## specific language governing permissions and limitations
 ## under the License.
 ##
+from datetime import (
+    datetime,
+    timezone,
+)
 from random import randint
 
+import pytest
 from django.urls import reverse_lazy
 from faker import Factory
 
@@ -381,3 +386,192 @@ class SuperPanelsTest(LoginGELUser):
         del parent2.panel.active_panel
         parent2 = parent2.panel.active_panel
         self.assertEqual(parent2.version, "0.3")
+
+    @pytest.mark.xfail(
+        reason="KMDS-1185: reported bug", strict=True, raises=AssertionError
+    )
+    def test_kmds_1185(self):
+        """Super Panel has no entities of its own"""
+        # Panel is created and starts life as a "normal" Panel (i.e not a super panel)
+
+        # Panel is public so that it is visible through the API
+        super_panel = GenePanelSnapshotFactory(
+            level4title__name="Neuromuscular disorders",
+            level4title__level3title="",
+            level4title__level2title="",
+            panel__status=GenePanel.STATUS.public,
+            panel__old_pk="",
+            old_panels=[],
+        )
+
+        # Panel has the AR_CAG STR added to it with a normal_repeats of 34
+        super_panel.add_str(
+            self.gel_user,
+            "AR_CAG",
+            {
+                "gene": None,
+                "chromosome": "1",
+                "position_37": [12345, 12346],
+                "position_38": [12345, 123456],
+                "normal_repeats": 34,
+                "pathogenic_repeats": 40,
+                "repeated_sequence": "CAG",
+                "sources": [],
+                "phenotypes": [],
+                "publications": [],
+                "moi": "Unknown",
+                "mode_of_pathogenicity": "",
+                "penetrance": GenePanelEntrySnapshot.PENETRANCE.Incomplete,
+                "current_diagnostic": False,
+            },
+        )
+
+        # A new Panel is created
+        child_panel = GenePanelSnapshotFactory(
+            level4title__name="Congenital myopathy",
+            level4title__level3title="",
+            level4title__level2title="",
+            panel__status=GenePanel.STATUS.public,
+            panel__old_pk="",
+            old_panels=[],
+        )
+        # Child Panel has the AR_CAG STR added to it with a normal_repeats of 35
+        child_panel.add_str(
+            self.gel_user,
+            "AR_CAG",
+            {
+                "gene": None,
+                "chromosome": "1",
+                "position_37": [12345, 12346],
+                "position_38": [12345, 123456],
+                "normal_repeats": 35,
+                "pathogenic_repeats": 40,
+                "repeated_sequence": "CAG",
+                "sources": [],
+                "phenotypes": [],
+                "publications": [],
+                "moi": "X-LINKED: hemizygous mutation in males, biallelic mutations in females",
+                "mode_of_pathogenicity": "",
+                "penetrance": GenePanelEntrySnapshot.PENETRANCE.Incomplete,
+                "current_diagnostic": False,
+            },
+        )
+
+        # The original Panel has the Child Panel added to it, making it a Super Panel
+        super_panel.child_panels.add(child_panel)
+
+        response = self.client.get(
+            reverse_lazy("api:v1:panels-detail", args=(super_panel.panel.pk,))
+        )
+        assert response.status_code == 200
+
+        assert response.json() == {
+            "id": 1,
+            "version": "0.1",
+            "name": "Neuromuscular disorders",
+            "hash_id": "",
+            "version_created": super_panel.created.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+            "status": "public",
+            "disease_group": "",
+            "disease_sub_group": "",
+            "relevant_disorders": [],
+            "types": [],
+            "stats": {
+                "number_of_genes": 0,
+                "number_of_regions": 0,
+                "number_of_strs": 1,
+            },
+            "genes": [],
+            "regions": [],
+            "strs": [
+                {
+                    "chromosome": "1",
+                    "confidence_level": "0",
+                    "entity_name": "AR_CAG",
+                    "entity_type": "str",
+                    "repeated_sequence": "CAG",
+                    "evidence": [],
+                    "gene_data": None,
+                    "grch37_coordinates": [12345, 12346],
+                    "grch38_coordinates": [12345, 123456],
+                    "mode_of_inheritance": "X-LINKED: hemizygous mutation in males, biallelic mutations in females",
+                    "normal_repeats": 35,
+                    "pathogenic_repeats": 40,
+                    "repeated_sequence": "CAG",
+                    "penetrance": "Incomplete",
+                    "phenotypes": [],
+                    "publications": [],
+                    "tags": [],
+                    "panel": {
+                        "id": 2,
+                        "version": "0.1",
+                        "name": "Congenital myopathy",
+                        "hash_id": "",
+                        "version_created": child_panel.created.strftime(
+                            "%Y-%m-%dT%H:%M:%S.%fZ"
+                        ),
+                        "status": "public",
+                        "disease_group": "",
+                        "disease_sub_group": "",
+                        "relevant_disorders": [],
+                        "types": [],
+                        "stats": {
+                            "number_of_genes": 0,
+                            "number_of_regions": 0,
+                            "number_of_strs": 1,
+                        },
+                    },
+                }
+            ],
+        }
+
+        response = self.client.get(
+            reverse_lazy("api:v1:entities-detail", args=("AR_CAG",))
+        )
+        assert response.status_code == 200
+
+        assert response.json() == {
+            "count": 2,
+            "next": None,
+            "previous": None,
+            "results": [
+                {
+                    "chromosome": "1",
+                    "confidence_level": "0",
+                    "entity_name": "AR_CAG",
+                    "entity_type": "str",
+                    "evidence": [],
+                    "gene_data": None,
+                    "grch37_coordinates": [12345, 12346],
+                    "grch38_coordinates": [12345, 123456],
+                    "mode_of_inheritance": "X-LINKED: hemizygous mutation in males, "
+                    "biallelic mutations in females",
+                    "normal_repeats": 35,
+                    "panel": {
+                        "disease_group": "",
+                        "disease_sub_group": "",
+                        "hash_id": "",
+                        "id": 2,
+                        "name": "Congenital myopathy",
+                        "relevant_disorders": [],
+                        "stats": {
+                            "number_of_genes": 0,
+                            "number_of_regions": 0,
+                            "number_of_strs": 1,
+                        },
+                        "status": "public",
+                        "types": [],
+                        "version": "0.1",
+                        "version_created": child_panel.created.strftime(
+                            "%Y-%m-%dT%H:%M:%S.%fZ"
+                        ),
+                    },
+                    "pathogenic_repeats": 40,
+                    "penetrance": "Incomplete",
+                    "phenotypes": [],
+                    "publications": [],
+                    "repeated_sequence": "CAG",
+                    "tags": [],
+                },
+            ],
+        }
