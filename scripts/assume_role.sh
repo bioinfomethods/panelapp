2e141c2ce44f45b9d5470e4ffb9e3f5c29f0dea8
MICHAELA O'HARA <ohara_michaela@hotmail.co.uk>
2022-09-07 11:52:47 +0100

    ci: refactor panelapp to use ECR instead of DockerHub

---
+++ scripts/assume_role.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# This script is not meant to be executed on it's own as you can't export vars
+# from a script. Instead source this file as part of your job in GitLab CI
+# foo:
+#   script:
+#     - source path/to/this/file
+
+aws sts assume-role \
+    --role-arn "${AWS_ASSUME_ROLE}" \
+    --role-session-name runner-session > aws_role
+
+AWS_ACCESS_KEY_ID=$(jq -r '.Credentials.AccessKeyId' aws_role)
+AWS_SECRET_ACCESS_KEY=$(jq -r '.Credentials.SecretAccessKey' aws_role)
+AWS_SESSION_TOKEN=$(jq -r '.Credentials.SessionToken' aws_role)
+
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+export AWS_SESSION_TOKEN=${AWS_SESSION_TOKEN}
